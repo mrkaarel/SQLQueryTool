ce5cce92cb6a66c22ce386552b7b1cd9e6b50d6c
Kaarel Julge <kaarel@gmail.com>
2015-01-12 11:49:16 +0200

    Moved data storage related code to a new class

--- Connections/ConnectionDataStorage.cs
+++ Connections/ConnectionDataStorage.cs
@@ -1,34 +1,22 @@
-﻿using System;
+﻿using SqlQueryTool.Utils;
+using System;
 using System.Collections.Generic;
-using System.IO;
-using System.IO.IsolatedStorage;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace SqlQueryTool.Connections
 {
 	public class ConnectionDataStorage
 	{
 		private static readonly string STORAGE_FILE_NAME = "SavedConnections";
-
-		private static readonly Encoding STORAGE_ENCODING = Encoding.UTF8;
 		private static readonly string STORAGE_ITEM_SEPARATOR = Environment.NewLine;
 
 		public static List<ConnectionData> LoadSavedSettings()
 		{
 			var previousSettings = new List<ConnectionData>();
-			using (var storageStream = OpenStorageStream(FileMode.OpenOrCreate)) {
-				var protectedSettings = ReadFully(storageStream);
-				
-				if (protectedSettings.Length == 0) {
-					return previousSettings;
-				}
 
-				var settingsString = STORAGE_ENCODING.GetString(ProtectedData.Unprotect(protectedSettings, null, DataProtectionScope.CurrentUser));
-				foreach (var serializedSettingsString in settingsString.Split(new string[] { STORAGE_ITEM_SEPARATOR }, StringSplitOptions.RemoveEmptyEntries )) {
-					previousSettings.Add(new ConnectionData(serializedSettingsString));
-				}
+			var settingsString = ProtectedDataStorage.ReadString(STORAGE_FILE_NAME);
+			foreach (var serializedSettingsString in settingsString.Split(new string[] { STORAGE_ITEM_SEPARATOR }, StringSplitOptions.RemoveEmptyEntries )) {
+				previousSettings.Add(new ConnectionData(serializedSettingsString));
 			}
 
 			return previousSettings;
@@ -59,30 +47,8 @@ namespace SqlQueryTool.Connections
 
 		private static void WriteSettingsToFile(IEnumerable<ConnectionData> settings)
 		{
-			using (var storageStream = OpenStorageStream(FileMode.Create)) {
-				string settingsString = String.Join(STORAGE_ITEM_SEPARATOR, settings.Select(s => s.SerializedString).ToArray());
-				var protectedSettings = ProtectedData.Protect(STORAGE_ENCODING.GetBytes(settingsString), null, DataProtectionScope.CurrentUser);
-
-				storageStream.Write(protectedSettings, 0, protectedSettings.Length);
-			}
-		}
-
-		private static byte[] ReadFully(Stream input)
-		{
-			byte[] buffer = new byte[16 * 1024];
-			using (MemoryStream ms = new MemoryStream()) {
-				int read;
-				while ((read = input.Read(buffer, 0, buffer.Length)) > 0) {
-					ms.Write(buffer, 0, read);
-				}
-				return ms.ToArray();
-			}
-		}
-
-		private static IsolatedStorageFileStream OpenStorageStream(FileMode openMode)
-		{
-			var userData = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly | IsolatedStorageScope.Domain, null, null);
-			return new IsolatedStorageFileStream(STORAGE_FILE_NAME, openMode, userData);
+			string settingsString = String.Join(STORAGE_ITEM_SEPARATOR, settings.Select(s => s.SerializedString).ToArray());
+			ProtectedDataStorage.WriteString(STORAGE_FILE_NAME, settingsString);
 		}
 	}
 }
---
+++ Utils/ProtectedDataStorage.cs
@@ -0,0 +1,97 @@
+﻿using System.IO;
+using System.IO.IsolatedStorage;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SqlQueryTool.Utils
+{
+	public class ProtectedDataStorage
+	{
+		private static readonly Encoding STORAGE_ENCODING = Encoding.UTF8;
+
+		public static string ReadString(string fileName)
+		{
+			return STORAGE_ENCODING.GetString(ReadBytes(fileName));
+		}
+
+		public static void WriteString(string fileName, string contents)
+		{
+			WriteBytes(fileName, STORAGE_ENCODING.GetBytes(contents));
+		}
+
+		public static object Read(string fileName)
+		{
+			var bytes = ReadBytes(fileName);
+
+			if (bytes.Length == 0) {
+				return null;
+			}
+
+			using (var ms = new MemoryStream(bytes)) {
+				return new BinaryFormatter().Deserialize(ms);
+			}
+		}
+
+		public static void Write(string fileName, object data)
+		{
+			using (var ms = new MemoryStream()) {
+				new BinaryFormatter().Serialize(ms, data);
+
+				WriteBytes(fileName, ms.ToArray());
+			}
+		}
+
+		public static void Delete(string fileName)
+		{
+			var storage = OpenStorage();
+			if (storage.GetFileNames(fileName).Length > 0) {
+				storage.DeleteFile(fileName);
+			}
+		}
+
+		private static void WriteBytes(string fileName, byte[] contents)
+		{
+			using (var storageStream = OpenStorageStream(fileName, FileMode.Create)) {
+				var protectedBytes = ProtectedData.Protect(contents, null, DataProtectionScope.CurrentUser);
+				storageStream.Write(protectedBytes, 0, protectedBytes.Length);
+			}
+		}
+
+		private static byte[] ReadBytes(string fileName)
+		{
+			using (var storageStream = OpenStorageStream(fileName, FileMode.OpenOrCreate)) {
+				var protectedBytes = ReadStreamToBytes(storageStream);
+
+				if (protectedBytes.Length == 0) {
+					return protectedBytes;
+				}
+
+				return ProtectedData.Unprotect(protectedBytes, null, DataProtectionScope.CurrentUser);
+			}
+		}
+
+		private static byte[] ReadStreamToBytes(Stream input)
+		{
+			byte[] buffer = new byte[16 * 1024];
+			using (MemoryStream ms = new MemoryStream()) {
+				int read;
+				while ((read = input.Read(buffer, 0, buffer.Length)) > 0) {
+					ms.Write(buffer, 0, read);
+				}
+				return ms.ToArray();
+			}
+		}
+
+		private static IsolatedStorageFileStream OpenStorageStream(string fileName, FileMode openMode)
+		{
+			return new IsolatedStorageFileStream(fileName, openMode, OpenStorage());
+		}
+
+		private static IsolatedStorageFile OpenStorage()
+		{
+			return IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly | IsolatedStorageScope.Domain, null, null);
+		}
+	}
+}
